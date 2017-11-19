bb52f6b5f30cd3b81a5b79d0078c03d55977bc30
Allison Chow <alchow@uwaterloo.ca>
2017-11-19 03:28:19 -0500

    fixed syntax and combined functions within put request

--- src/Sidebar/Sidebar1.js
+++ src/Sidebar/Sidebar1.js
@@ -14,76 +14,72 @@ class MenuExampleSecondaryPointing extends Component {
 
   constructor() {
     super();
-    this.state = {clicked: false, inputPanel: <Segment>
-    <ExpenseTable>
-    </ExpenseTable>
-    </Segment>};
+    this.state = {
+      clicked: false, inputPanel: <Segment>
+        <ExpenseTable>
+        </ExpenseTable>
+      </Segment>
+    };
     this.handleItemClick1 = this.handleItemClick1.bind(this);
     this.handleItemClick2 = this.handleItemClick2.bind(this);
     this.handleItemClick3 = this.handleItemClick3.bind(this);
   }
 
-  
+
   render() {
     var weatherNetworkBaseUrl = "https://hackathon.pic.pelmorex.com/api";
     var precipUrl = "https://hackathon.pic.pelmorex.com/api/precip/date?locationcode=CAON0383&locale=en-CA";
     var dataShort = "https://hackathon.pic.pelmorex.com/api/data/shortterm?locationcode=CAON0383";
-    
-    axios.get(weatherNetworkBaseUrl+'/search/string?keyword=London&prov=ON')
-    .then(function (res) {
-      var locationCode = res.data.code;
+
+    axios.get(weatherNetworkBaseUrl + '/search/string?keyword=London&prov=ON')
+      .then(function (res) {
+        var locationCode = res.data.code;
         console.log(locationCode);
-    })
-    .catch(console.log)  
+      })
+      .catch(console.log)
 
     axios.get(precipUrl)
-    .then(function (res) {
-      var verylongspeech=res.data.speech;
+      .then(function (res) {
+        var verylongspeech = res.data.speech;
         console.log(verylongspeech);
-    })
-    .catch(console.log) 
+      })
+      .catch(console.log)
     axios.get(dataShort)
-    .then(function (res) {
-      var dataValue=res.data;
+      .then(function (res) {
+        var dataValue = res.data;
+        var dataList = [100];
+        var suggestionList = [100];
+  
+        for (var i = 0; i < dataValue.length; i++) {
+          if (res.dat.period == 3) {
+            dataList[i] = dataValue.date;
+          }
+          if (dataValue.rain >= 0.7) {
+            suggestionList[i] = "There will be a high chance of rain today! Please dress appropriately and bring an umbrella in case.";
+          }
+          if (dataValue.snow >= 0.7) {
+            suggestionList[i] = "There will be a high chance of snow today! Please wear a jacket and your winter boots.";
+          }
+        }
+        for (var i = 0; i < dataList.length; i++) {
+          console.log(dataList[i]);
+          console.log(suggestionList[i]);
+        }
         //console.log(dataValue);
-    })
-    .catch(console.log) 
-    
-    weatherPeriodData(){
-    var dataList = [100]; 
-    var suggestionList = [100]; 
-
-    for(var i=0;i<dataValue.length;i++){
-      if(res.dat.period==3){
-        dataList[i]=dataValue.date;
-      }
-        if(dataValue.rain>=0.7){
-          suggestionList[i] = "There will be a high chance of rain today! Please dress appropriately and bring an umbrella in case.";
-        }
-        if(dataValue.snow>=0.7){
-          suggestionList[i] = "There will be a high chance of snow today! Please wear a jacket and your winter boots.";
-        }
-    }
-    for(var i=0;i<dataList.length;i++){
-      console.log(dataList[i]);
-      console.log(suggestionList[i]);
-    }
-    }
-    
-
-
+      })
+      .catch(console.log)
 
     return (
       <div>
         <Menu pointing secondary>
           <Menu.Item name='Expense Page' onClick={this.handleItemClick1} />
           <Menu.Item name='Owe Page' onClick={this.handleItemClick2} />
-          <Menu.Item name='Budgeting'  onClick={this.handleItemClick3} />
+          <Menu.Item name='Budgeting' onClick={this.handleItemClick3} />
           <Menu.Menu position='right'>
-          <Login></Login>
+            <Login></Login>
           </Menu.Menu>
         </Menu>
-      
+
         {this.state.inputPanel}
 
 
@@ -91,36 +87,42 @@ class MenuExampleSecondaryPointing extends Component {
     )
   }
   handleItemClick1() {
-    this.setState({clicked: true,inputPanel: 
-    <Segment>
-    <ExpenseTable>
-    </ExpenseTable>
+    this.setState({
+      clicked: true, inputPanel:
+        <Segment>
+          <ExpenseTable>
+          </ExpenseTable>
 
-    <ExpenseBarChart>
-    </ExpenseBarChart>
-    <ExpensePieChart>
-    </ExpensePieChart>
+          <ExpenseBarChart>
+          </ExpenseBarChart>
+          <ExpensePieChart>
+          </ExpensePieChart>
 
-    </Segment>});
-}
-handleItemClick2() {
-  this.setState({clicked: true,inputPanel: 
-  <Segment>
-  <Friends>
-  </Friends>
-  <OweChart>
-    </OweChart>
-  </Segment>});
-}
-handleItemClick3() {
-  this.setState({clicked: true,inputPanel: 
-  <Segment>
-  <Suggestion>
-  </Suggestion>
-  <SuggestionBarChart>
-  </SuggestionBarChart>
-  </Segment>});
-}
+        </Segment>
+    });
+  }
+  handleItemClick2() {
+    this.setState({
+      clicked: true, inputPanel:
+        <Segment>
+          <Friends>
+          </Friends>
+          <OweChart>
+          </OweChart>
+        </Segment>
+    });
+  }
+  handleItemClick3() {
+    this.setState({
+      clicked: true, inputPanel:
+        <Segment>
+          <Suggestion>
+          </Suggestion>
+          <SuggestionBarChart>
+          </SuggestionBarChart>
+        </Segment>
+    });
+  }
 }
 
 
